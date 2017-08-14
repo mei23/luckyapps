ab049fadc1edf4bb353180d00cfd2e36f363bad8
mei23 <m@m544.net>
2017-08-14 19:51:18 +0900

    my domain

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000
 
 const config = {
   PORT: port,
-  URL: prod ? 'https://luckyapps.herokuapp.com' : `http://localhost:${port}`,
+  URL: prod ? 'https://meiapps.herokuapp.com' : `http://localhost:${port}`,
 }
 
 module.exports = config
