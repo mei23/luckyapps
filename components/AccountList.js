4750048301e2fa468057a3844eacf1db1398175b
mei23 <m@m544.net>
2017-08-22 06:24:15 +0900

    AccountList style

--- components/AccountList.js
+++ components/AccountList.js
@@ -1,6 +1,9 @@
 
 import * as U from '../utils/utils'
 
+/**
+ * User icon with link
+ */
 const AvatarIcon = (props) => {
   const acc = props.account
   const size = props.size > 0 ? props.size : 48
@@ -10,26 +13,39 @@ const AvatarIcon = (props) => {
     <a href={acc.url} title={acc.display_name} target='_blank'>
       <img className='avatar'
         src={U.resolveAvatarUrl(props.host, acc.avatar)}
-        style={{'width':`${size}px`, 'height':`${size}px`, 'border-radius':`${radius}px`}}
+        style={{ width:`${size}px`, height:`${size}px`, borderRadius:`${radius}px` }}
         role='presentation' />
     </a>
   )
 }
 
+/**
+ * Active users part
+ */
 export default (props) => {
   const users = props.users
-  return (<div>
-    <div>アクティブユーザー({users.length}人)</div>
-    <div style={{ display:'flex', flexWrap: 'wrap' }}>
-      {users.map(user => (
-        <div style={{ margin:'2px' }}>
-          <div style={{ padding:'0px', margin:'0px'}}><AvatarIcon account={user.obj} size='24' /></div>
-          <div style={{'text-align':'right', marginTop:'-16px',
-            textShadow: '0px 0px 2px #eee', color: '#000'
-          }}>{user.cnt}</div>
-        </div>
-      ))}
-    </div>
-
-  </div>)
+  return (
+    <div className='active-users'>
+      <div>アクティブユーザー({users.length}人)</div>
+      <div className='user-list' style={{ display: 'flex', flexWrap: 'wrap' }}>
+        {users.map(user => (
+          <div className='user'>
+            <div className='user-icon'>
+              <AvatarIcon account={user.obj} size='24' />
+            </div>
+            <div className='user-toot'>{user.cnt}</div>
+          </div>
+        ))}
+      </div>
+      <style jsx>{`
+        .user { margin: 2px; }
+        .user-icon { margin: 0px; padding: 0px; }
+        .user-toot {
+          text-align: right;
+          margin-top: -16px;
+          color: #000;
+          textShadow: 0px 0px 2px #eee;
+        }
+      `}</style>
+    </div>)
 }
